8d797781d55a5c216ecc0c3f0992a02f6167b0b2
notnaughty20 <rafailfataliev01@gmail.com>
2022-06-21 18:50:18 +0300

    Initial Commit

---
+++ JSONTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JSONTest
+//
+//  Created by Рафаил Фаталиев on 21.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
