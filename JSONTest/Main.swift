6d464fe4d2bb804ec2ae92a64a6c8deef3040242
notnaughty20 <rafailfataliev01@gmail.com>
2022-06-22 16:49:52 +0300

    first commit

---
+++ JSONTest/Main.swift
@@ -0,0 +1,8 @@
+//
+//  Main.swift
+//  JSONTest
+//
+//  Created by Рафаил Фаталиев on 22.06.2022.
+//
+
+import Foundation
---
+++ JSONTest/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  JSONTest
+//
+//  Created by Рафаил Фаталиев on 21.06.2022.
+//
+
+import Foundation
