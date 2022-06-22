af95dddd94a283e4a2e1c7d73ad48dab8a56a041
notnaughty20 <85493269+notnaughty20@users.noreply.github.com>
2022-06-22 16:51:41 +0300

    Update Main.swift

--- JSONTest/Main.swift
+++ JSONTest/Main.swift
@@ -6,3 +6,62 @@
 //
 
 import Foundation
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+       super.viewDidLoad()
+    
+
+
+let jsonString = """
+{
+    "data": [{
+            "name": "hz",
+            "data": {
+                "text": "Текстовый блок"
+            }
+        }, {
+            "name": "picture",
+            "data": {
+                "url": "https://pryaniky.com/static/img/logo-a-512.png",
+                "text": "Пряники"
+            }
+        }, {
+            "name": "selector",
+            "data": {
+                "selectedId": 1,
+                "variants": [{
+                        "id": 1,
+                        "text": "Вариант раз"
+                    }, {
+                        "id": 2,
+                        "text": "Вариант два"
+                    }, {
+                        "id": 3,
+                        "text": "Вариант три"
+                    }
+                ]
+            }
+        }
+    ],
+    "view": ["hz", "selector", "picture", "hz"]
+}
+"""
+
+let jsonData = jsonString.data(using: .utf8)!
+
+let rootModel = try! JSONDecoder().decode(Root.self, from: jsonData)
+
+print("Data:\n")
+rootModel.data.forEach { print($0, "\n") }
+
+    print("Views:\n")
+        print(rootModel.view)
+        
+        
+        
+    }
+}
+
