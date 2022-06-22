6da300ebfd0d655211e645bf0ee1a6204f915861
notnaughty20 <85493269+notnaughty20@users.noreply.github.com>
2022-06-22 16:52:18 +0300

    Update Model.swift

--- JSONTest/Model.swift
+++ JSONTest/Model.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+
+struct Root: Codable {
+    var data: [DataModel]
+    var view: [String]
+}
+
+struct DataModel: Codable {
+    var name: String
+    var data: NestedData
+}
+
+extension DataModel: CustomStringConvertible {
+    var description: String { "name: \(name)\ndata: \(data)" }
+}
+
+struct NestedData: Codable {
+    var text: String?
+    var url: String?
+    var selectedId: Int?
+    var variants: [Variant]?
+}
+
+extension NestedData: CustomStringConvertible {
+    var description: String {
+        return "text: \(text), url: \(url), selectedId: \(selectedId), variants: \(variants)"
+    }
+}
+
+struct Variant: Codable {
+    var id: Int
+    var text: String
+}
+
